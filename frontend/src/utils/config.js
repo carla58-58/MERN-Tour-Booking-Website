08642d2e689f8b6146191938508b45935fc1d8d0
carla58-58 <carlaa_58@hotmail.com>
2025-06-18 00:25:11 +0800

    config.js modify

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:4000/api/v1";
+//export const BASE_URL = "http://localhost:4000/api/v1";
 //change this to deploy on render.com or any other hosting service
-//export const BASE_URL = "https://mern-tour-booking-website-backend.onrender.com/api/v1";
+export const BASE_URL = "https://mern-tour-booking-website-backend.onrender.com/api/v1";
